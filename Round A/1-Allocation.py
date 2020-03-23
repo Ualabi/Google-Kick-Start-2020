c20e50aaf2659c15bf4ba434fc4ce479b2e5aadf
Joseandres Hinojoza <jhinojoza@outlook.com>
2020-03-23 00:36:53 -0600

    1 and 2

---
+++ Round A/1-Allocation.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for x in range(T):
+    [n,k] = list(map(int,input().split()))
+    arr = sorted(list(map(int,input().split())))
+    y = 0
+    while y<len(arr) and arr[y] <= k:
+        k -= arr[y]
+        y += 1
+    
+    print('Case #{}: {}'.format(x+1,y))
---
+++ Round A/2-Plates.py
@@ -0,0 +1,21 @@
+#solucion
+def getSol(N,K,P):
+    dp = []
+    for x in range(N+1):  
+        dp.append([0]*(P+1))
+    
+    for x in range(N):
+        dp[x+1] = dp[x][:]
+        arr = list(map(int,input().split()))
+        s = 0
+        for y in range(K):
+            s += arr[y]
+            for z in range(P-y):
+                dp[x+1][z+y+1] = max(dp[x][z]+s, dp[x+1][z+y+1])                
+    return dp[N][P]
+
+T = int(input())
+for x in range(T):
+    [N,K,P] = list(map(int,input().split()))
+    resp = getSol(N,K,P)
+    print('Case #{}: {}'.format(x+1,resp))
